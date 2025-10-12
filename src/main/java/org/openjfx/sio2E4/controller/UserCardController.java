8b87ca5da60ed35b3f1c0a59817dff26d4324284
Foxelou <97977229+Foxelou@users.noreply.github.com>
2025-10-12 17:52:10 +0200

    refactor: amélioration du LocalStorageService avec ajout des méthodes update find, correction du remove et intégration dans UsersController

--- src/main/java/org/openjfx/sio2E4/controller/UserCardController.java
+++ src/main/java/org/openjfx/sio2E4/controller/UserCardController.java
@@ -21,6 +21,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import org.openjfx.sio2E4.service.LocalStorageService;
+import org.openjfx.sio2E4.service.NetworkService;
 
 public class UserCardController {
 
@@ -43,22 +45,39 @@ public class UserCardController {
     private final String BEARER_TOKEN = "Bearer "+ AuthService.getToken();
     
     public void loadUser(int userId) {
-        HttpClient client = HttpClient.newHttpClient();
-        String urlString = "http://localhost:8080/api/users/" + userId;
+        if (NetworkService.isOnline()) {
+            HttpClient client = HttpClient.newHttpClient();
+            String urlString = "http://localhost:8080/api/users/" + userId;
 
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(urlString))
-            .header("Authorization", BEARER_TOKEN)
-            .GET()
-            .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(urlString))
+                    .header("Authorization", BEARER_TOKEN)
+                    .GET()
+                    .build();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-            .thenApply(HttpResponse::body)
-            .thenAccept(this::parseUserResponse)
-            .exceptionally(e -> {
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(this::parseUserResponse)
+                    .exceptionally(e -> {
+                        e.printStackTrace();
+                        return null;
+                    });
+        } else {
+            User user = LocalStorageService.findUserById(userId);
+            try {
+                Platform.runLater(() -> {
+                    // Affichage des informations utilisateur dans les labels
+                    nomLabel.setText(user.getNom());
+                    prenomLabel.setText(user.getPrenom());
+                    emailLabel.setText(user.getEmail());
+                    telephoneLabel.setText(user.getTelephone());
+                    adresseLabel.setText(user.getAdresse());
+                    roleLabel.setText(user.getRole().getLibelle());
+                });
+            } catch (NullPointerException e) {
                 e.printStackTrace();
-                return null;
-            });
+            }
+        }
     }
 
     private void parseUserResponse(String response) {
